bbc6fec5d228de59a9333a8193d765f5309f5bf5
Timothy-Achonu <achonutimothy@gmail.com>
2022-11-16 16:08:36 +0100

    creating calculteAnswer function

--- src/App.js
+++ src/App.js
@@ -8,23 +8,46 @@ class App extends Component {
     super(props);
 
     this.state = {
-      content: "",
-      answer : "",
+      content: "0",
+      answer: "",
     };
   }
-  getButtonContent = (e) => {
-     console.log(e.target.textContent, 'Clicked')
-     this.setState(prevContent => {
-      if(e.target.textContent === "=") {
-        return {...this.state,
-           answer : 111}
-
+  calculateAnswer() {
+    let numsArray = this.state.content.split(" ");
+    let result;
+    numsArray.forEach((item, index) => {
+      if (!Number(item)) {
+       result = numsArray[index - 1] + numsArray[index + 1];
       }
-     return (
-      {...this.state, content : this.state.content+e.target.textContent}
-     )
-    })
+    });
+    console.log(numsArray);
   }
+  getButtonContent = (e) => {
+    this.setState((prevContent) => {
+      if (e.target.textContent === "C") {
+        return { content: "0", answer: "" };
+      }
+      if (e.target.textContent === "=") {
+        this.calculateAnswer();
+        return { ...this.state, answer: 111 };
+      }
+      if (this.state.content === "0") {
+        return { ...this.state, content: e.target.textContent };
+      }
+      if (!Number(e.target.textContent)) {
+        if (e.target.textContent === "+") {
+        }
+        return {
+          ...this.state,
+          content: this.state.content + " " + e.target.textContent + " ",
+        };
+      }
+      return {
+        ...this.state,
+        content: this.state.content + e.target.textContent,
+      };
+    });
+  };
 
   render() {
     const buttonValues = [
@@ -48,16 +71,18 @@ class App extends Component {
 
     const buttons = buttonValues.map((button, index) => {
       return (
-        <Button textContent={button} 
-        handleClick={this.getButtonContent} 
-        key={index}/>
+        <Button
+          textContent={button}
+          handleClick={this.getButtonContent}
+          key={index}
+        />
       );
     });
     return (
       <div className="App">
         <h1>Calculator</h1>
         <div className="caculator-structure">
-          <Screen values={this.state}/>
+          <Screen values={this.state} />
           <div className="button-wrapper">{buttons}</div>
         </div>
       </div>
